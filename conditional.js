3fdc2d896af6e919c3e356379bb5428e6be32461
alpine-dc <dcalpine99@gmail.com>
2021-03-06 15:06:39 +0700

    try conditional

---
+++ conditional.js
@@ -0,0 +1,21 @@
+var x = 10;
+
+if(x === 1){
+    console.log("Angka 1")
+}else if(x === 2){
+    console.log("Angka 2")
+}else {
+    console.log("Angka bukan 1 dan 2")
+}
+
+//Jika nilai dibawah 100, maka tidak maximal
+//Jika nilai diatas 100, maka maximal
+
+var nilai = 75
+if(nilai < 100){
+    console.log("Tidak Maximal")
+}else if(nilai > 100){
+    console.log("Maximal")
+}
+
+//
---
+++ index.js
@@ -0,0 +1,30 @@
+var num1 = 10;
+var num2 = "15";
+
+var total = num1 + num2
+// console.log(total)
+
+
+// concat
+// var subject = "Alpine"
+// var tobe = "is"
+// var verbing = "learning"
+// var object = "JavaScript"
+
+// var sentence = subject + tobe + verbing + object
+// console.log(sentence)
+
+// console.log(String(num1))
+// console.log(Number(num2))
+
+// OPERATOR LOGIC
+// AND, OR , NOT, ==, ===
+
+// var x = 10;
+// var y = "10";
+
+// var a = x === y;
+// var b = x == y;
+// var c = a != b;
+// console.log(c)
+
